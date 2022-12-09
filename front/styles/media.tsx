8549e3d59e6d5b7a1ab05b00ed1d04111864b283
mirrer <alsejr1004@gmail.com>
2022-12-09 23:03:09 +0900

    Fix: Responsive Size

--- front/styles/media.tsx
+++ front/styles/media.tsx
@@ -1,9 +1,6 @@
-const breakPoints = [768, 1200];
+const breakPoints = [992];
+const [desktop] = breakPoints.map(bp => `@media (min-width: ${bp}px)`);
 
-const mobile = `@media (min-width: ${breakPoints[0]}px)`;
-const desktop = `@media (min-width: ${breakPoints[1]}px)`;
-
-const media = { mobile, desktop };
-// const media = breakPoints.map(bp => `@media (min-width: ${bp}px)`);
+const media = { desktop };
 
 export default media;
