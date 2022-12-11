659091ef17e05e3150e420001673e36355f6831d
mirrer <alsejr1004@gmail.com>
2022-12-11 21:14:27 +0900

    Add: Signup Success Modal

--- front/components/Modal/signup.tsx
+++ front/components/Modal/signup.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
 
 const Modal = () => {
   return (
     <>
-      <div>모달</div>
-      <div>두두등장</div>
+      <FontAwesomeIcon icon={faXmark} />
+      <FontAwesomeIcon icon={faCircleCheck} />
+      <header>Success!</header>
+      <div>회원가입이 정상적으로 완료되었습니다.</div>
+      <Link to="/login">
+        <button>Start H to H</button>
+      </Link>
     </>
   );
 };
