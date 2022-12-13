c8716154c021f7e9963e0433008559206ec4bd2d
mirrer <alsejr1004@gmail.com>
2022-12-13 15:44:20 +0900

    Add: Layout of Channel and Message Pages

--- front/layouts/Workspace.tsx
+++ front/layouts/Workspace.tsx
@@ -1,9 +1,17 @@
 import React, { FC, useCallback } from 'react';
+import { Redirect, Switch, Route } from 'react-router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faSquarePlus, faPenToSquare } from '@fortawesome/free-regular-svg-icons';
 import useSWR from 'swr';
 import axios from 'axios';
+import gravatar from 'gravatar';
+import loadable from '@loadable/component';
 
 import fetcher from '@utils/fetcher';
-import { Redirect } from 'react-router';
+
+const Channel = loadable(() => import('@pages/channel'));
+const Message = loadable(() => import('@pages/message'));
 
 const Workspace: FC = ({ children }) => {
   const { data, error, revalidate, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
@@ -23,10 +31,80 @@ const Workspace: FC = ({ children }) => {
   }
 
   return (
-    <div>
-      <button onClick={onLogout}>로그아웃</button>
-      {children}
-    </div>
+    <>
+      {/* <button onClick={onLogout}>로그아웃</button> */}
+      <div>
+        <header>H to H</header>
+        <button>
+          <img src={gravatar.url(data.email, { s: '28px', d: 'mm' })} alt={data.nickname} />
+        </button>
+      </div>
+
+      <label id="search-label">
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+        <input type="text" placeholder="Search messages or users" />
+      </label>
+
+      <div>
+        <button>
+          <img src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt="chanel1" />
+        </button>
+        <button>
+          <img src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt="chanel1" />
+        </button>
+        <button>
+          <img src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt="chanel1" />
+        </button>
+        <button>
+          <FontAwesomeIcon icon={faSquarePlus} />
+        </button>
+      </div>
+
+      <div>
+        <button>
+          <FontAwesomeIcon icon={faAngleDown} />
+          Channels
+        </button>
+        <button># 일반</button>
+        <button>
+          <FontAwesomeIcon icon={faSquarePlus} />
+          Add channels
+        </button>
+      </div>
+
+      <div>
+        <button>
+          <FontAwesomeIcon icon={faAngleDown} />
+          Direct message
+        </button>
+        <button>
+          <img src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt="chanel1" />
+          Slackbot
+        </button>
+        <button>
+          <img src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt="chanel1" />
+          zerocho
+        </button>
+        <button>
+          <img src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt="chanel1" />
+          zerocho1
+        </button>
+
+        <button>
+          <FontAwesomeIcon icon={faSquarePlus} />
+          Add teammates
+        </button>
+      </div>
+
+      <header>NodeJS KR Developer Group</header>
+      <FontAwesomeIcon icon={faAngleDown} />
+      <FontAwesomeIcon icon={faPenToSquare} />
+
+      <Switch>
+        <Route path="/workspace/channel" component={Channel} />
+        <Route path="/workspace/message" component={Message} />
+      </Switch>
+    </>
   );
 };
 
