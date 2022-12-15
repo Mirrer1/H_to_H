0d9c18297b6b7cacba2e2fda129a686cac38c3eb
mirrer <alsejr1004@gmail.com>
2022-12-15 14:28:30 +0900

    Add: Invite Channel Modal

---
+++ front/components/Modal/InviteChannel.tsx
@@ -0,0 +1,88 @@
+import React, { useCallback } from 'react';
+import { useParams } from 'react-router';
+import { toast } from 'react-toastify';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import useSWR from 'swr';
+import axios from 'axios';
+
+import useInput from '@hooks/useInput';
+import fetcher from '@utils/fetcher';
+import { IUser } from '@typings/db';
+import {
+  CreateForm,
+  CreateFormBtn,
+  CreateFormItem,
+  CreateWrapper,
+  CreateXBtn,
+} from '@styles/ComponentsStyle/Modal/createWorkspace';
+
+interface Props {
+  setInviteChannelVisible: () => void;
+  onClickProfile: () => void;
+}
+
+const InviteChannel = ({ setInviteChannelVisible, onClickProfile }: Props) => {
+  const [newMember, onChangeNewMember, setNewMember] = useInput('');
+  const { workspace, channel } = useParams<{ workspace: string; channel: string }>();
+
+  const { data: userData } = useSWR<IUser>('/api/users', fetcher);
+  const { revalidate: revalidateMembers } = useSWR<IUser[]>(
+    userData && channel ? `/api/workspaces/${workspace}/channels/${channel}/members` : null,
+    fetcher,
+  );
+
+  const onInviteMember = useCallback(
+    e => {
+      e.preventDefault();
+
+      if (!newMember || !newMember.trim()) {
+        return;
+      }
+
+      axios
+        .post(
+          `/api/workspaces/${workspace}/channels/${channel}/members`,
+          {
+            email: newMember,
+          },
+          {
+            withCredentials: true,
+          },
+        )
+        .then(() => {
+          setNewMember('');
+          revalidateMembers();
+          setInviteChannelVisible();
+          onClickProfile();
+        })
+        .catch(error => {
+          console.dir(error);
+          toast.error(error.response?.data, { position: 'top-center' });
+        });
+    },
+    [newMember],
+  );
+  return (
+    <CreateWrapper>
+      <CreateXBtn onClick={setInviteChannelVisible}>
+        <FontAwesomeIcon icon={faXmark} />
+      </CreateXBtn>
+
+      <CreateForm>
+        <form onSubmit={onInviteMember}>
+          <CreateFormItem id="member-label">
+            <div>채널 멤버 초대</div>
+            <input id="member" value={newMember} onChange={onChangeNewMember} />
+          </CreateFormItem>
+
+          <CreateFormBtn type="submit" mainBtn>
+            초대
+          </CreateFormBtn>
+        </form>
+      </CreateForm>
+    </CreateWrapper>
+  );
+};
+
+export default InviteChannel;
--- front/components/Modal/Profile.tsx
+++ front/components/Modal/Profile.tsx
@@ -6,9 +6,10 @@ import gravatar from 'gravatar';
 import fetcher from '@utils/fetcher';
 import CreateChannel from '@components/Modal/CreateChannel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import InviteWorkspace from '@components/Modal/InviteWorkspace';
+import InviteChannel from '@components/Modal/InviteChannel';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { ProfileWrapper, ProfileContent, ProfileBtn } from '@styles/ComponentsStyle/Modal/profile';
-import InviteWorkspace from './InviteWorkspace';
 
 interface Props {
   onClickProfile: () => void;
@@ -17,6 +18,7 @@ interface Props {
 const Profile = ({ onClickProfile }: Props) => {
   const { data, error, revalidate, mutate } = useSWR('/api/users', fetcher);
   const [inviteWorkspaceVisible, setInviteWorkspaceVisible] = useState(false);
+  const [inviteChannelVisible, setInviteChannelVisible] = useState(false);
   const [createChannelVisible, setCreateChannelVisible] = useState(false);
 
   const onLogout = useCallback(() => {
@@ -31,6 +33,10 @@ const Profile = ({ onClickProfile }: Props) => {
 
   const onClickInviteWorkspace = useCallback(() => {
     setInviteWorkspaceVisible(prev => !prev);
+  }, []);
+
+  const onClickInviteChannel = useCallback(() => {
+    setInviteChannelVisible(prev => !prev);
   }, []);
 
   const onClickCreateChannel = useCallback(() => {
@@ -51,13 +57,17 @@ const Profile = ({ onClickProfile }: Props) => {
 
       <ProfileBtn>
         <button onClick={onClickInviteWorkspace}>워크스페이스에 사용자 초대</button>
-        {/* <button>채널에 사용자 초대</button> */}
+        <button onClick={onClickInviteChannel}>채널에 사용자 초대</button>
         <button onClick={onClickCreateChannel}>채널 생성</button>
         <button onClick={onLogout}>로그아웃</button>
       </ProfileBtn>
 
       {inviteWorkspaceVisible && (
         <InviteWorkspace setInviteWorkspaceVisible={onClickInviteWorkspace} onClickProfile={onClickProfile} />
+      )}
+
+      {inviteChannelVisible && (
+        <InviteChannel setInviteChannelVisible={onClickInviteChannel} onClickProfile={onClickProfile} />
       )}
 
       {createChannelVisible && (
